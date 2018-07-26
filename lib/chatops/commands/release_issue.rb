70a93221307f546f98513fa4c821fef9a996781a
Robert Speicher <rspeicher@gmail.com>
2018-07-26 11:06:35 -0700

    Add a ReleaseIssue command

---
+++ lib/chatops/commands/release_issue.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+module Chatops
+  module Commands
+    # Triggers a pipeline in release-tools that creates a release task issue for
+    # a specified version.
+    #
+    # This command is context-aware and will create a specific type of task
+    # issue based on where the command was run:
+    #
+    # - In #security, it will create a security task issue
+    # - Anywhere else, it will create either a monthly issue or a patch issue,
+    #   depending on the version.
+    #
+    # See https://gitlab.com/gitlab-org/release-tools/blob/master/doc/chatops.md#release-issues
+    class ReleaseIssue
+      include Command
+      include Release::Command
+
+      VERSION_REGEX = /\A\d+\.\d+\.(?<patch>\d+)(-rc(?<rc>\d+))?\z/
+
+      usage "#{command_name} [VERSION]"
+      description 'Create a task issue for a specified version.'
+
+      def perform
+        version = required_argument(0, 'version')
+        validate_version!(version)
+
+        if channel == 'security'
+          task = 'security_patch_issue'
+        else
+          matches = version.match(VERSION_REGEX)
+
+          task =
+            if matches[:patch].to_i.zero? && matches[:rc].nil?
+              'monthly_issue'
+            else
+              'patch_issue'
+            end
+        end
+
+        trigger_release(version, task)
+      end
+
+      private
+
+      def validate_version!(version)
+        return if VERSION_REGEX.match?(version)
+
+        raise ArgumentError, "Invalid version provided: #{version}"
+      end
+
+      def channel
+        super
+      rescue KeyError
+        # Some tests don't provide the channel environment variable. That's ok.
+        'releases'
+      end
+    end
+  end
+end
---
+++ spec/chatops/commands/release_issue_spec.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe Chatops::Commands::ReleaseIssue, :release_command do
+  describe '#perform' do
+    include_context 'release command #perform'
+
+    it 'raises an error when no argument is given' do
+      expect { described_class.new.perform }
+        .to raise_error(ArgumentError, 'You must specify the version!')
+    end
+
+    it 'validates the provided version string', :aggregate_failures do
+      valid   = %w[10.9.0 10.9.1 10.9.0-rc1]
+      invalid = %w[10.9.0-ee 10.9.0-rc1-ee 10.9 10.9.0-rc]
+
+      valid.each do |version|
+        expect { stubbed_instance(version).perform }
+          .not_to raise_error
+      end
+
+      invalid.each do |version|
+        expect { stubbed_instance(version).perform }
+          .to raise_error(ArgumentError, "Invalid version provided: #{version}")
+      end
+    end
+
+    context 'with a security release' do
+      it 'runs the trigger' do
+        expect(stubbed_client).to trigger_release(
+          RELEASE_VERSION: '10.9.0',
+          TASK: 'security_patch_issue'
+        )
+
+        instance = stubbed_instance('10.9.0')
+        # Stub the channel since we don't set the environment variable
+        expect(instance).to receive(:channel).and_return('security')
+
+        instance.perform
+      end
+    end
+
+    context 'with a monthly release' do
+      it 'runs the trigger' do
+        expect(stubbed_client).to trigger_release(
+          RELEASE_VERSION: '10.9.0',
+          TASK: 'monthly_issue'
+        )
+
+        stubbed_instance('10.9.0').perform
+      end
+    end
+
+    context 'with a patch release' do
+      it 'runs the trigger' do
+        expect(stubbed_client).to trigger_release(
+          RELEASE_VERSION: '10.9.0-rc5',
+          TASK: 'patch_issue'
+        )
+
+        stubbed_instance('10.9.0-rc5').perform
+      end
+    end
+
+    include_examples 'with a valid chatops job',    version: '10.9.0'
+    include_examples 'with an invalid chatops job', version: '10.9.0'
+  end
+end
