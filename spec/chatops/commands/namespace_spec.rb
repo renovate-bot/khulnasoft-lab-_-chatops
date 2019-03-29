4202d8f104fcbfcf2733735cf62299027ec16da3
cmcfarland <cmcfarland@gitlab.com>
2019-03-29 13:04:44 -0400

    Better tests?

--- spec/chatops/commands/namespace_spec.rb
+++ spec/chatops/commands/namespace_spec.rb
@@ -4,11 +4,19 @@ require 'spec_helper'
 
 describe Chatops::Commands::Namespace do
   describe '#perform' do
+    context 'without a namespace id' do
+      it 'returns an error message' do
+        command = described_class.new(%w[])
+
+        expect(command.perform).to eq('You must supply a namespace ID to look up.')
+      end
+    end
+
     context 'when passed a namespace ID' do
       it 'tries to look it up' do
-        command = described_class.new(%w[1234567])
+        command = described_class.new(%w[1234567], {}, 'GITLAB_TOKEN' => '123')
 
-        expect(command)
+        expect(command.perform)
           .to receive(:find)
           .with('1234567')
 
