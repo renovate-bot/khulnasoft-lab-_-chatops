acf601244c950afa01e9183563b9fc310b95a9c5
cmcfarland <cmcfarland@gitlab.com>
2019-03-29 11:07:33 -0400

    Building out responses.

--- lib/chatops/commands/namespace.rb
+++ lib/chatops/commands/namespace.rb
@@ -26,6 +26,53 @@ module Chatops
           namespace_not_found_error(namespace_id)
         end
       end
+
+      def submit_namespace_details(namespace)
+        Slack::Message
+          .new(token: slack_token, channel: channel)
+          .send(
+            attachments: [
+              {
+                fields: [
+                  {
+                    title: 'ID',
+                    value: namespace.id,
+                    short: true
+                  },
+                  {
+                    title: 'Name',
+                    value: namespace.name,
+                    short: true
+                  },
+                  {
+                    title: 'Owner ID',
+                    value: namespace.owner_id,
+                    short: true
+                  },
+                  {
+                    title: 'Path',
+                    value: namespace.path,
+                    short: true
+                  },
+                  {
+                    title: 'Created At',
+                    value: namespace.created_at,
+                    short: true
+                  },
+                  {
+                    title: 'Last Updated At',
+                    value: namespace.updated_at,
+                    short: true
+                  }
+                ]
+              }
+            ]
+          )
+      end
+
+      def namespace_not_found_error(namespace_id)
+        "No namespace could be found for the id #{namespace_id.inspect}."
+      end
     end
   end
 end
