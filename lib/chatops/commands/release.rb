26e376f684cdf2dde2c85e539206a47fc6213a6b
Balasankar "Balu" C <balasankar@gitlab.com>
2019-08-19 15:30:42 +0530

    Add chatops command to create stable branch
    Signed-off-by: Balasankar "Balu" C <balasankar@gitlab.com>

--- lib/chatops/commands/release.rb
+++ lib/chatops/commands/release.rb
@@ -10,7 +10,7 @@ module Chatops
       usage "#{command_name} SUBCOMMAND [OPTIONS]"
       description 'Perform release-related tasks.'
 
-      COMMANDS = Set.new(%w[issue merge prepare qa status tag])
+      COMMANDS = Set.new(%w[issue merge prepare qa stable_branch status tag])
 
       options do |o|
         o.bool '--security',
@@ -55,6 +55,10 @@ module Chatops
             Create a QA issue for changes between 1.2.0-rc1 and 1.2.0-rc3
 
               release qa 1.2.0-rc1 1.2.0-rc3
+
+            Create stable branches for 1.2.0 from master branch
+
+              releae stable_branch 1.2.0 master
 
             Tag 1.2.3 as a security release
 
@@ -124,6 +128,12 @@ module Chatops
         validate_comparison!(tags)
 
         trigger_release(tags.join(','), "#{namespace}:#{__method__}")
+      end
+
+      def stable_branch(version)
+        validate_version!(version) unless options[:security]
+
+        trigger_release(version, "#{namespace}:#{__method__}")
       end
 
       def status(version = nil)
