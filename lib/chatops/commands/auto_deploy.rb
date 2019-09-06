2f10a622b0a8262b08e6b932a2cdab60571f50f0
Robert Speicher <rspeicher@gmail.com>
2019-09-06 11:57:15 -0500

    Remove bold styling from auto_deploy commit SHA

--- lib/chatops/commands/auto_deploy.rb
+++ lib/chatops/commands/auto_deploy.rb
@@ -113,7 +113,7 @@ module Chatops
           blocks << {
             type: 'section',
             text: Slack.markdown(
-              "*`#{commit_link(commit.short_id)}`* #{commit.title}"
+              "`#{commit_link(commit.short_id)}` #{commit.title}"
             )
           }
 
