4b695bc33d6e78f1a5575fb486a8cf1392b4c42e
cmcfarland <cmcfarland@gitlab.com>
2019-10-28 15:12:45 -0400

    Initial work on new deploycmd command for chatops.

---
+++ lib/chatops/commands/deploycmd.rb
@@ -0,0 +1,83 @@
+# frozen_string_literal: true
+
+###
+# Much of this is lifted from the deploy command.
+###
+
+module Chatops
+  module Commands
+    class Deploycmd
+      include Command
+
+      usage "#{command_name} [COMMAND NAME] [ROLE]"
+      description 'Runs ansible commands across roles in our fleet.'
+
+      options do |o|
+        o.bool('--production', 'Run command in production instead of staging.')
+        o.bool('--dr', 'Run command in DR instead of staging.')
+        o.bool('--canary', 'Run command in canary.')
+        o.bool('--pre', 'Run command in PRE instead of staging.')
+      end
+
+      def perform
+        command_name = arguments[0]
+        role = arguments[1]
+        vars = {
+          'CMD': command_name,
+          'GITLAB_ROLE': role,
+          'CURRENT_DEPLOY_ENVIRONMENT': environment,
+          'CHECKMODE': '--check'
+        }
+        response = client.run_trigger(
+          trigger_project,
+          trigger_token,
+          :master,
+          vars
+        )
+
+        url = response.web_url
+
+        "The command was issued: <#{url}>"
+      rescue StandardError => error
+        "The command could not be run: #{error.message}"
+      end
+
+      def client
+        # For triggers we don't need an API token, so we explicitly set it to
+        # nil.
+        @client ||= Gitlab::Client.new(token: nil, host: trigger_host)
+      end
+
+      def trigger_token
+        env.fetch('COMMAND_TRIGGER_TOKEN')
+      end
+
+      def trigger_project
+        env.fetch('COMMAND_TRIGGER_PROJECT')
+      end
+
+      def trigger_host
+        env.fetch('COMMAND_TRIGGER_HOST')
+      end
+
+      def environment
+        base =
+          if options[:production]
+            'gprd'
+          elsif options[:pre]
+            'pre'
+          elsif options[:dr]
+            'dr'
+          else
+            'gstg'
+          end
+
+        if options[:canary]
+          "#{base}-cny"
+        else
+          base
+        end
+      end
+    end
+  end
+end
---
+++ spec/chatops/commands/deploycmd_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe Chatops::Commands::Deploycmd do
+  describe '#perform' do
+    it 'requires tests' do
+      raise 'You must write tests for the #perform method'
+    end
+  end
+end
