707155c74fbeeca22ce63128e0a639e9a128d6e7
cmcfarland <cmcfarland@gitlab.com>
2019-10-30 11:10:01 -0400

    Initial rspec file. Liberally stolen from deploy command.

--- spec/chatops/commands/deploycmd_spec.rb
+++ spec/chatops/commands/deploycmd_spec.rb
@@ -3,9 +3,75 @@
 require 'spec_helper'
 
 describe Chatops::Commands::Deploycmd do
-  describe '#perform' do
-    it 'requires tests' do
-      raise 'You must write tests for the #perform method'
+  describe '.perform' do
+    it 'supports a --production option' do
+      instance = instance_double('instance')
+
+      expect(described_class)
+        .to receive(:new)
+        .with(%w[], a_hash_including(production: true), {})
+        .and_return(instance)
+
+      expect(instance)
+        .to receive(:perform)
+
+      described_class.perform(%w[--production])
+    end
+
+    it 'supports a --canary option' do
+      instance = instance_double('instance')
+
+      expect(described_class)
+        .to receive(:new)
+        .with(%w[], a_hash_including(canary: true), {})
+        .and_return(instance)
+
+      expect(instance)
+        .to receive(:perform)
+
+      described_class.perform(%w[--canary])
+    end
+
+    it 'supports a --dr option' do
+      instance = instance_double('instance')
+
+      expect(described_class)
+        .to receive(:new)
+        .with(%w[], a_hash_including(dr: true), {})
+        .and_return(instance)
+
+      expect(instance)
+        .to receive(:perform)
+
+      described_class.perform(%w[--dr])
+    end
+
+    it 'supports a --pre option' do
+      instance = instance_double('instance')
+
+      expect(described_class)
+        .to receive(:new)
+        .with(%w[], a_hash_including(pre: true), {})
+        .and_return(instance)
+
+      expect(instance)
+        .to receive(:perform)
+
+      described_class.perform(%w[--pre])
+    end
+
+    it 'supports a --list' do
+      instance = instance_double('instance')
+
+      expect(described_class)
+        .to receive(:new)
+        .with(%w[], a_hash_including(list: true), {})
+        .and_return(instance)
+
+      expect(instance)
+        .to receive(:perform)
+
+      described_class.perform(%w[--list])
     end
   end
 end
