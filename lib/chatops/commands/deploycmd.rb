50e0a03ce5b49e9669ffdfc4be91cf8abb8e1f2d
cmcfarland <cmcfarland@gitlab.com>
2019-10-31 14:03:00 -0400

    Remove a puts for troubleshooting.

--- lib/chatops/commands/deploycmd.rb
+++ lib/chatops/commands/deploycmd.rb
@@ -51,7 +51,6 @@ module Chatops
         }
         vars[:CHECKMODE] = '--check' unless options[:no_check]
         vars[:ANSIBLE_SKIP_TAGS] = 'haproxy' if options[:skip_haproxy]
-        puts vars
         response = client.run_trigger(
           trigger_project,
           trigger_token,
