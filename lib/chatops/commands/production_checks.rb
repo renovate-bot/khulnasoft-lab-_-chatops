4d6c23e12772eb5eae579308f7716b512a5c87a1
John Jarvis <jarv@gitlab.com>
2021-05-07 13:45:26 +0200

    Adds a chatops command to run production checks

---
+++ lib/chatops/commands/production_checks.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module Chatops
+  module Commands
+    class ProductionChecks
+      include Command
+      include ::Chatops::Release::Command
+
+      description 'Performs a production check and displays the result'
+
+      def perform
+        run_trigger(CHECK_PRODUCTION: 'true')
+
+        'Production checks triggered, the results will appear shortly.'
+      end
+    end
+  end
+end
---
+++ spec/chatops/commands/production_checks_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe Chatops::Commands::ProductionChecks do
+  describe '#perform' do
+    let(:command) do
+      described_class.new(
+        [],
+        {},
+        'SLACK_TOKEN' => '123',
+        'CHAT_CHANNEL' => '456'
+      )
+    end
+    let(:slack_msg) { instance_spy(Chatops::Slack::Message) }
+
+    it 'performs a production check' do
+      expect(command).to receive(:run_trigger).with(CHECK_PRODUCTION: 'true')
+      expect(command.perform)
+        .to eq('Production checks triggered, the results will appear shortly.')
+    end
+  end
+end
