33d94f9738f836518425f6bd087395a19a780f49
Reuben Pereira <rpereira@gitlab.com>
2022-02-22 14:32:02 +0530

    Support Omnibus project in ReleaseCheck::Commit

--- lib/chatops/gitlab/release_check/commit.rb
+++ lib/chatops/gitlab/release_check/commit.rb
@@ -4,7 +4,11 @@ module Chatops
   module Gitlab
     module ReleaseCheck
       class Commit
-        AUTO_DEPLOY_BRANCH_REGEX = /^\d+-\d+-auto-deploy-\d+$/
+        # 14-8-auto-deploy-2022020906
+        GITLAB_AUTO_DEPLOY_BRANCH_REGEX = /^\d+-\d+-auto-deploy-\d+$/
+
+        # 14.8.202202082218+113b5654b3c.d8c9987d59d
+        OMNIBUS_AUTO_DEPLOY_TAG_REGEX = /^\d+\.\d+\.\d+\+\h+\.\h+$/
 
         def initialize(client, project, commit_sha)
           @client = client
@@ -16,11 +20,11 @@ module Chatops
         #
         # @return [Boolean]
         def deployed_to_gprd?
-          branches_containing_sha = auto_deploy_branches_containing_commit(sha)
+          refs_containing_sha = auto_deploy_refs_containing_commit(sha)
 
           gprd_environment_status.any? do |environment|
-            branches_containing_sha.any? do |auto_deploy_branch|
-              environment[:branch] == auto_deploy_branch.name && environment[:status] == 'success'
+            refs_containing_sha.any? do |auto_deploy_ref|
+              environment[:branch] == auto_deploy_ref.name && environment[:status] == 'success'
             end
           end
         end
@@ -30,11 +34,11 @@ module Chatops
         attr_reader :client, :project, :sha
 
         def gprd_environment_status
-          rails = Gitlab::Deployments
+          deployments = Gitlab::Deployments
             .new(client, project)
             .upcoming_and_current('gprd')
 
-          rails.map do |ee|
+          deployments.map do |ee|
             {
               role: 'gprd',
               revision: (ee&.short_sha || 'unknown'),
@@ -44,10 +48,28 @@ module Chatops
           end
         end
 
-        def auto_deploy_branches_containing_commit(sha)
+        def auto_deploy_refs_containing_commit(sha)
           client
-            .refs_containing_commit(project: project, type: 'branch', sha: sha)
-            .select { |b| b.name.match?(AUTO_DEPLOY_BRANCH_REGEX) }
+            .refs_containing_commit(project: project, type: ref_type, sha: sha)
+            .select { |ref| ref.name.match?(auto_deploy_regex) }
+        end
+
+        def auto_deploy_regex
+          @auto_deploy_regex ||=
+            if Projects::GITLAB_SECURITY_PROJECT == project
+              GITLAB_AUTO_DEPLOY_BRANCH_REGEX
+            elsif Projects::OMNIBUS_SECURITY_PROJECT == project
+              OMNIBUS_AUTO_DEPLOY_TAG_REGEX
+            end
+        end
+
+        def ref_type
+          @ref_type ||=
+            if Projects::GITLAB_SECURITY_PROJECT == project
+              'branch'
+            elsif Projects::OMNIBUS_SECURITY_PROJECT == project
+              'tag'
+            end
         end
       end
     end
--- spec/chatops/gitlab/release_check/commit_spec.rb
+++ spec/chatops/gitlab/release_check/commit_spec.rb
@@ -7,10 +7,10 @@ describe Chatops::Gitlab::ReleaseCheck::Commit do
     subject(:deployed_to_gprd) { described_class.new(gitlab, project, sha).deployed_to_gprd? }
 
     let(:sha) { 'sha' }
-    let(:project) { 'gitlab-org/security/gitlab' }
+    let(:gitlab_project) { 'gitlab-org/security/gitlab' }
     let(:gitlab) { instance_spy(Chatops::Gitlab::Client) }
 
-    let(:production_status) do
+    let(:gitlab_production_status) do
       instance_double(
         'deployment',
         role: 'gprd',
@@ -28,7 +28,7 @@ describe Chatops::Gitlab::ReleaseCheck::Commit do
       deployments = instance_double(Chatops::Gitlab::Deployments)
       allow(Chatops::Gitlab::Deployments)
         .to receive(:new)
-        .with(gitlab, 'gitlab-org/security/gitlab')
+        .with(gitlab, project)
         .and_return(deployments)
 
       allow(deployments)
@@ -37,24 +37,60 @@ describe Chatops::Gitlab::ReleaseCheck::Commit do
         .and_return([production_status])
     end
 
-    it 'returns false if commit has not been deployed to gprd' do
-      allow(gitlab)
-        .to receive(:refs_containing_commit)
-        .with(project: 'gitlab-org/security/gitlab', sha: sha, type: 'branch')
-        .and_return([])
+    context 'with gitlab project' do
+      let(:project) { gitlab_project }
+      let(:production_status) { gitlab_production_status }
 
-      expect(deployed_to_gprd).to eq(false)
+      it 'returns false if commit has not been deployed to gprd' do
+        allow(gitlab)
+          .to receive(:refs_containing_commit)
+          .with(project: gitlab_project, sha: sha, type: 'branch')
+          .and_return([])
+
+        expect(deployed_to_gprd).to eq(false)
+      end
+
+      it 'returns true if commit has been deployed to gprd' do
+        branches_with_commit = [instance_double('branch', type: 'branch', name: gitlab_production_status.ref)]
+
+        allow(gitlab)
+          .to receive(:refs_containing_commit)
+          .with(project: gitlab_project, sha: sha, type: 'branch')
+          .and_return(branches_with_commit)
+
+        expect(deployed_to_gprd).to eq(true)
+      end
     end
 
-    it 'returns true if commit has been deployed to gprd' do
-      branches_with_commit = [instance_double('branch', type: 'branch', name: production_status.ref)]
+    context 'with omnibus project' do
+      let(:project) { omnibus_project }
+      let(:omnibus_project) { 'gitlab-org/security/omnibus-gitlab' }
+      let(:production_status) { omnibus_production_status }
 
-      allow(gitlab)
-        .to receive(:refs_containing_commit)
-        .with(project: 'gitlab-org/security/gitlab', sha: sha, type: 'branch')
-        .and_return(branches_with_commit)
+      let(:omnibus_production_status) do
+        instance_double(
+          'deployment',
+          role: 'gprd',
+          version: '14.8.0-pre',
+          revision: '0874a8d346c',
+          ref: '14.8.202202220320+62b2458151c.bef3527e358',
+          package: '14.8.202202220320-62b2458151c.bef3527e358',
+          sha: 'long-sha',
+          status: 'success',
+          short_sha: '0874a8d346c'
+        )
+      end
 
-      expect(deployed_to_gprd).to eq(true)
+      it 'returns true if commit has been deployed to gprd' do
+        tags_with_commit = [instance_double('tag', type: 'tag', name: omnibus_production_status.ref)]
+
+        allow(gitlab)
+          .to receive(:refs_containing_commit)
+          .with(project: omnibus_project, sha: sha, type: 'tag')
+          .and_return(tags_with_commit)
+
+        expect(deployed_to_gprd).to eq(true)
+      end
     end
   end
 end
