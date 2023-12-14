18797ce02ccfa3eee425e426996b46bda24caeff
Reuben Pereira <2967854-rpereira2@users.noreply.gitlab.com>
2023-12-14 08:48:47 +0000

    Upgrade chef to v16.18 to fix image building

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'vcr'
+require 'rexml/document' # Without this, we get an "uninitialized constant REXML::ParseException"
 
 VCR.configure do |c|
   c.cassette_library_dir = File.expand_path('../fixtures', __dir__)
